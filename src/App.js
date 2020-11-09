ecdaa1466d1846451d08ca38cde8002a8ae12fdb
StartMeUpLille <contact@start-me-up.fr>
2020-11-09 13:32:33 +0100

    basic css, components, page layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Categories from "./components/Categories";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+      <section id="header">
+        <div className="container">
+          <Header />
+        </div>
+      </section>
+      <section id="hero">
+        <div className="container">
+          <Hero />
+        </div>
+      </section>
+      <section id="content">
+        <div className="container">
+          <div id="menus">
+            <Categories />
+          </div>
+          <div id="sidebar">
+            <Cart />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
